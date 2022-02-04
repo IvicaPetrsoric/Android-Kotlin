ab91c256db473c8d3641554efbd7df4695d4c9bf
Ivica Petrsoric <ivica.petrsoric@gmail.com>
2022-02-05 00:14:55 +0100

    setup undo

--- app/src/main/java/com/example/androidkotlin/MainActivity.kt
+++ app/src/main/java/com/example/androidkotlin/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.androidkotlin
 
+import android.Manifest
 import android.app.Dialog
+import android.content.Intent
 import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
 
@@ -15,6 +24,37 @@ class MainActivity : AppCompatActivity() {
 
     private var mDrawingView: DrawingView? = null
     private var mImageButtonCurrentPaint: ImageButton? = null
+
+    val openGalleryLauncher: ActivityResultLauncher<Intent> =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+              result ->
+                if (result.resultCode == RESULT_OK && result.data != null) {
+                    val imageBackground: ImageView = findViewById(R.id.iv_background)
+                    imageBackground.setImageURI(result.data?.data)
+                }
+
+    }
+
+    val requestPermission: ActivityResultLauncher<Array<String>> =
+            registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
+                permissions ->
+                permissions.entries.forEach {
+                    val permissionName = it.key
+                    val isGranted = it.value
+
+                    if (isGranted) {
+                        Toast.makeText(this, "Permission granted", Toast.LENGTH_LONG).show()
+
+                        val pickIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                        openGalleryLauncher.launch(pickIntent)
+                    } else {
+                        if (permissionName == Manifest.permission.READ_EXTERNAL_STORAGE) {
+                            Toast.makeText(this, "Permission not granted", Toast.LENGTH_LONG).show()
+                        }
+                    }
+                }
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +73,27 @@ class MainActivity : AppCompatActivity() {
         val ib_brush: ImageButton = findViewById(R.id.ib_brush)
         ib_brush.setOnClickListener {
             showBrushSizeChooserDialog()
+        }
+
+        val ibGallery: ImageButton = findViewById(R.id.ib_gallery)
+        ibGallery.setOnClickListener {
+            requestStoragePermission()
+        }
+
+        val ibUndo: ImageButton = findViewById(R.id.ib_undo)
+        ibUndo.setOnClickListener {
+            mDrawingView?.onClickUndo()
+        }
+    }
+
+    private fun requestStoragePermission() {
+        if(ActivityCompat.shouldShowRequestPermissionRationale(
+                        this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            showRationaleDialog("Drawing App", "Need acces!!!!")
+        } else {
+            requestPermission.launch(arrayOf(
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+            ))
         }
     }
 
@@ -78,5 +139,15 @@ class MainActivity : AppCompatActivity() {
 
             mImageButtonCurrentPaint = view
         }
+    }
+
+    private fun showRationaleDialog(title: String, message: String) {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle(title)
+                .setMessage(message)
+                .setPositiveButton("Cancel") { dialog, _ ->
+                    dialog.dismiss()
+                }
+        builder.create().show()
     }
 }
