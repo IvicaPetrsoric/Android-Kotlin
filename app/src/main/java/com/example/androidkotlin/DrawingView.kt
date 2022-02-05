674ee3132bc92da70bcc4e19830c46c6dbe24e19
Ivica Petrsoric <ivica.petrsoric@gmail.com>
2022-02-05 01:13:05 +0100

    Kotlin12MasterClass: setup drawing app

--- app/src/main/java/com/example/androidkotlin/DrawingView.kt
+++ app/src/main/java/com/example/androidkotlin/DrawingView.kt
@@ -7,6 +7,7 @@ import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
 import java.lang.reflect.Type
+import java.util.*
 
 class DrawingView(context: Context, attrs: AttributeSet): View(context, attrs) {
 
