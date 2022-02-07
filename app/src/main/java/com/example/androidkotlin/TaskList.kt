88f14901856c4e80135ab501842d667fa3707529
Ivica Petrsoric <ivica.petrsoric@gmail.com>
2022-02-07 18:19:29 +0100

    Kotlin12MasterClass: learning RecycleView

---
+++ app/src/main/java/com/example/androidkotlin/TaskList.kt
@@ -0,0 +1,11 @@
+package com.example.androidkotlin
+
+object TaskList {
+    val taskList = listOf<Task>(
+        Task("Take a Walk", "7:00 am"),
+        Task("Take a Walk 2", "8:00 am"),
+        Task("Take a Walk 3", "9:00 am"),
+        Task("Take a Walk 4", "10:00 am"),
+        Task("Take a Walk 5", "11:00 am")
+        )
+}
